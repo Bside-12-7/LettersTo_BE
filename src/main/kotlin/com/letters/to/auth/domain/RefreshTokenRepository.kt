ae5fac136603187fa4e9263a039600f6c12b6c3c
lalize <printf@kakao.com>
2022-10-08 01:08:15 +0900

    카카오 로그인 기능 추가

--- src/main/kotlin/com/letters/to/auth/domain/RefreshTokenRepository.kt
+++ src/main/kotlin/com/letters/to/auth/domain/RefreshTokenRepository.kt
@@ -1,9 +1,10 @@
 package com.letters.to.auth.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
 
 interface RefreshTokenRepository : JpaRepository<RefreshToken, String> {
     fun deleteByAccessToken(accessToken: String)
 }
 
-fun RefreshTokenRepository.findByRefreshToken(refreshToken: String): RefreshToken? = findById(refreshToken).orElse(null)
+fun RefreshTokenRepository.findByRefreshToken(refreshToken: String): RefreshToken? = findByIdOrNull(refreshToken)

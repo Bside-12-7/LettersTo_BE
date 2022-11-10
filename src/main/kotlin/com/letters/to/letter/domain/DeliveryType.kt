825f0a3de391a964d264aa9d6b30360140be1d69
lalize <printf@kakao.com>
2022-11-10 20:12:16 +0900

    ktlint

--- src/main/kotlin/com/letters/to/letter/domain/DeliveryType.kt
+++ src/main/kotlin/com/letters/to/letter/domain/DeliveryType.kt
@@ -8,13 +8,11 @@ private const val UNIT = 2000
 
 enum class DeliveryType(val stampCount: Int, private val delegate: BiFunction<Member, Member, LocalDateTime>) {
     NONE(0, { _, _ -> LocalDateTime.now() }),
-    NORMAL(1,
-        { from, to ->
-            val meter = from.geolocation.haversine(to.geolocation)
-            LocalDateTime.now().plusMinutes((meter / UNIT).toLong())
-        }),
-    EXPRESS(5,
-        { _, _ -> LocalDateTime.now() });
+    NORMAL(1, { from, to ->
+        val meter = from.geolocation.haversine(to.geolocation)
+        LocalDateTime.now().plusMinutes((meter / UNIT).toLong())
+    }),
+    EXPRESS(5, { _, _ -> LocalDateTime.now() });
 
     fun deliveryDate(fromMember: Member, toMember: Member): LocalDateTime {
         return delegate.apply(fromMember, toMember)

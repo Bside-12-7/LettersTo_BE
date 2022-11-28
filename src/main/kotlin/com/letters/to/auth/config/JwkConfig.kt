ef3b190cd2e37e6706ccffe53e3070857b146120
lalize <printf@kakao.com>
2022-11-28 20:21:59 +0900

    애플 로그인 기능 추가

--- src/main/kotlin/com/letters/to/auth/config/JwkConfig.kt
+++ src/main/kotlin/com/letters/to/auth/config/JwkConfig.kt
@@ -5,6 +5,7 @@ import com.auth0.jwk.JwkProviderBuilder
 import com.letters.to.auth.domain.ProviderType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.net.URL
 import java.util.concurrent.TimeUnit
 
 @Configuration
@@ -13,6 +14,13 @@ class JwkConfig {
     @Bean
     fun kakaoJwkProvider(): Pair<ProviderType, JwkProvider> {
         return ProviderType.KAKAO to JwkProviderBuilder("https://kauth.kakao.com")
+            .cached(10, 5, TimeUnit.HOURS)
+            .build()
+    }
+
+    @Bean
+    fun appleJwkProvider(): Pair<ProviderType, JwkProvider> {
+        return ProviderType.APPLE to JwkProviderBuilder(URL("https://appleid.apple.com/auth/keys"))
             .cached(10, 5, TimeUnit.HOURS)
             .build()
     }
--- src/main/kotlin/com/letters/to/auth/domain/ProviderType.kt
+++ src/main/kotlin/com/letters/to/auth/domain/ProviderType.kt
@@ -1,7 +1,8 @@
 package com.letters.to.auth.domain
 
 enum class ProviderType(val issuer: String) {
-    KAKAO("https://kauth.kakao.com");
+    KAKAO("https://kauth.kakao.com"),
+    APPLE("https://appleid.apple.com");
 
     companion object {
         fun findByIssuer(issuer: String): ProviderType {

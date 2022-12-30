0f066e7739cd960e4cc55235efad58ed1b6b8a04
lalize <printf@kakao.com>
2022-12-30 14:34:16 +0900

    편지 내용 길이 제한 수정

--- src/main/kotlin/com/letters/to/letter/domain/Content.kt
+++ src/main/kotlin/com/letters/to/letter/domain/Content.kt
@@ -3,6 +3,6 @@ package com.letters.to.letter.domain
 @JvmInline
 value class Content(val value: String) {
     init {
-        require(value.length <= 500) { "내용은 500자를 초과할 수 없습니다." }
+        require(value.length in (100..1000)) { "편지 내용 길이는 100~500자여야 합니다." }
     }
 }
